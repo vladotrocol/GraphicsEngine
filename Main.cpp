a1c1b5faa147cf8e425693b56831a1ee75e28a39
Vlad Otrocol <vladotrocol@yahoo.com>
2013-07-30 17:40:01 +0100

    sierpinsky

--- Main.cpp
+++ Main.cpp
@@ -7,29 +7,48 @@
 #include <GL/glut.h>
 #endif
 
+struct GLintPoint{
+	GLint x,y;
+};
+
+int random(int m){
+	return rand()%m;
+};
+
+void drawDot(GLint x, GLint y){
+	glBegin(GL_POINTS);
+		glVertex2i(x,y);
+	glEnd();
+};
+
 void gs_init(){
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glColor3f(0.0f, 0.0f, 0.0f);
-	glPointSize(4.0);
+	glPointSize(2.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+	gluOrtho2D(0.0, 800.0, 0.0, 600.0);
 };
 
 void gs_render(){
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POINTS);
-		glVertex2i(100, 50);
-		glVertex2i(100, 130);
-		glVertex2i(150, 130);
-	glEnd();
+	GLintPoint T[3] = {{10,10},{600,10},{300,600}};
+	int index = random(3);
+	GLintPoint point = T[index];
+	drawDot(point.x, point.y);
+	for(int i=0;i<55000;i++){
+		index = random(3);
+		point.x = (point.x+T[index].x)/2;
+		point.y = (point.y+T[index].y)/2;
+		drawDot(point.x, point.y);
+	}
 	glFlush();
 };
 
 void main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(640,480);
+	glutInitWindowSize(800,600);
 	glutInitWindowPosition(100,150);
 	glutCreateWindow("Graphics Engine");
 	glutDisplayFunc(gs_render);
