5a7557791290fd19a49088aac859758e9a48f5ef
Vlad Otrocol <vladotrocol@yahoo.com>
2013-07-31 20:07:32 +0100

    cool line sphere

--- Main.cpp
+++ Main.cpp
@@ -6,33 +6,73 @@
 #define FREEGLUT_STATIC
 #include <GL/glut.h>
 #endif
+#include <vector>
+#include <time.h> 
+using namespace std;
+const float pi = 3.14159265348979;
+
+typedef struct Pointe{
+	GLfloat x;
+	GLfloat y;
+}Point;
+
+vector<Point> Points;
+Point p;
+int size = 30;
+float r;
+float g;
+float b;
 
 void gs_init(){
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glColor3f(0.0f, 0.0f, 0.0f);
+	srand (time(NULL));
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glColor3f(1.0f, 0.0f, 0.0f);
 	glPointSize(4.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+	gluOrtho2D(0.0, 600.0, 0.0, 600.0);
+
+	for(int i = 1; i <= size; i++){
+		p.x = 280*cos(pi/size*2*i) + 300;
+		p.y = 280*sin(pi/size*2*i) + 300;
+		Points.push_back(p);
+	}
+};
+
+float random(){
+	return (float)rand()/(float)RAND_MAX;
 };
 
 void gs_render(){
 	glClear(GL_COLOR_BUFFER_BIT);
+	for(unsigned int i= 0; i<Points.size();i++){
+		r = random();
+		g = random();
+		b = random();
+		glColor3f(r,g,b);
+		for(unsigned int j= 0; j<Points.size();j++){
+			if(i!=j){
+				glBegin(GL_LINES);
+				glVertex2f(Points[i].x, Points[i].y);
+				glVertex2f(Points[j].x, Points[j].y);
+				glEnd();
+			}
+		}
+	}
 	glBegin(GL_POINTS);
-		glVertex2i(100, 50);
-		glVertex2i(100, 130);
-		glVertex2i(150, 130);
+		glVertex2f(300.0f, 300.0f);
 	glEnd();
-	glFlush();
+	glutSwapBuffers();
 };
 
 void main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(640,480);
+	glutInitWindowSize(600,600);
 	glutInitWindowPosition(100,150);
 	glutCreateWindow("Graphics Engine");
-	glutDisplayFunc(gs_render);
+	glutIdleFunc(gs_render);
+	glutDisplayFunc(gs_render);	
 	gs_init();
 	glutMainLoop();
 };
